3889b0ff5a0e9f1b8bdb4ce15e97fef2a0fd0d6e
harpagun333a <matorz@student.agh.edu.pl>
2019-12-14 20:54:38 +0100

    cwiczenie 2 done

---
+++ Cwiczenie_01_Indeksy_i_Zakresy/Cwiczenie_01_Indeksy_i_Zakresy/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace Cwiczenie_01_Indeksy_i_Zakresy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cwiczenie 1\n");
+
+            string[] slowa = new string[]
+            {
+            // Indeks               od początku     od końca
+            "Już północ",           // 0            ^10
+            "cień",                 // 1            ^9
+            "ponury",               // 2            ^8
+            "pół",                  // 3            ^7
+            "świata",               // 4            ^6
+            "okrywa",               // 5            ^5
+            "A jeszcze",            // 6            ^4
+            "serce",                // 7            ^3
+            "zmysłom",              // 8            ^2
+            "spoczynku nie daje"    // 9            ^1
+                                    // 10(słowa.Lenght) ^0
+
+            };
+
+            // =================================================================
+            Console.WriteLine("Pkt. 1\n");
+            Console.WriteLine($"{slowa[^1]}");
+
+            // =================================================================
+            Console.WriteLine("\nPkt. 2\n");
+            string[] sonet1 = slowa[2..6];
+            foreach (var slowo in sonet1)
+                Console.Write($"< { slowo} >");
+
+            Console.WriteLine();
+
+            // =================================================================
+            Console.WriteLine("\nPkt. 3\n");
+            string[] sonet2 = slowa[^3..^0];
+            foreach (var slowo in sonet2)
+                Console.Write($"{slowo}");
+            Console.WriteLine();
+
+            // =================================================================
+            Console.WriteLine("\nPkt. 4\n");
+            string[] sonet3 = slowa[..];
+            string[] sonet4 = slowa[..5];
+            string[] sonet5 = slowa[7..];
+
+            foreach (var slowo in sonet3)
+                Console.Write($"{slowo}");
+            Console.WriteLine();
+
+            foreach (var slowo in sonet4)
+                Console.Write($"{slowo}");
+            Console.WriteLine();
+
+            foreach (var slowo in sonet5)
+                Console.Write($"{slowo}");
+            Console.WriteLine();
+
+            // =================================================================
+            Console.WriteLine("\nPkt. 5\n");
+            Index stri = ^5;
+            Console.WriteLine(slowa[stri]);
+
+
+            // =================================================================
+            Console.WriteLine("\nPkt. 6\n");
+            Range fraza = 2..7;
+            string[] tekst = slowa[fraza];
+            foreach (var slowo in tekst)
+                Console.Write($" {slowo} ");
+            Console.WriteLine();
+
+        }
+    }
+}
---
+++ Cwiczenie_02_Events/Cwiczenie_02_Events/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+namespace Cwiczenie_02_Events
+{
+    class Program
+    {
+        public delegate void cyfra();
+        public delegate void znak();
+
+        public event cyfra Digit;
+        public event cyfra Character;
+
+        protected virtual void OnDigit()
+        {
+            if (Digit != null)
+                Digit();
+            else
+                Console.WriteLine("Naciśnięto cyfrę!");
+        }
+
+        protected virtual void OnCharacter()
+        {
+            if (Character != null)
+                Character();
+            else
+                Console.WriteLine("Naciśnięto literę!");
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cwiczenie 2\n");
+
+            ConsoleKeyInfo key;
+            Ewenty ev = new Ewenty();
+            ev.OnDigit += to_cyfra;
+            ev.OnCharacter += to_litera;
+
+
+            while(true)
+            {
+                key = Console.ReadKey();
+                Console.WriteLine("\b" + key.KeyChar);
+                if(!ev.CheckSign(key.KeyChar))
+                {
+                    break;
+                }
+                
+            }
+
+        }
+
+        public static void to_cyfra()
+        {
+            Console.WriteLine("Naciśnięto cyfrę!");
+        }
+
+        public static void to_litera()
+        {
+            Console.WriteLine("Naciśnięto literę!");
+        }
+    }
+
+    class Ewenty
+    {
+        public delegate void cyfra();
+        public delegate void znak();
+
+        public event cyfra OnDigit;
+        public event znak OnCharacter;
+
+        public bool CheckSign(char c)
+        {
+            if(Char.IsDigit(c))
+            {
+                if(OnDigit != null)
+                {
+                    OnDigit();
+                    return true;
+                }
+            }
+            else if(Char.IsLetter(c))
+            {
+                if(OnCharacter != null)
+                {
+                    OnCharacter();
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
